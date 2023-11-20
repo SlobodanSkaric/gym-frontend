9f60ad0c1f9dcff7ea1b0a10966f430ade083f0f
Slobodan <slobodan.skaric@gmail.com>
2023-11-20 21:52:06 +0100

    Add react icon, work in the main branch,is not ok:)

--- src/components/elemnts/NavBar.jsx
+++ src/components/elemnts/NavBar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MenuLink from "../../menuLinks.js";
 import { NavLink } from 'react-router-dom';
+import { BiAlignRight } from "react-icons/bi";
 
 function NavBar() {
+    const [menuButton, setMenuButton] = useState(false);
+    console.log(menuButton);
   return (
-    
+    <>
     <div className='sm:flex justify-end items-center hidden'>
         {MenuLink.map((element, index) =>(
             <div className='sm:text-lg text-gray-300   hover:bg-sky-800 rounded-lg'>
@@ -12,6 +15,10 @@ function NavBar() {
             </div>
         ))}
     </div>
+    <div className='sm:hidden flex justify-end '>
+        <BiAlignRight className='w-[32px] h-[32px] text-cyan-100 hover:cursor-pointer' onClick={() => setMenuButton((current) => !current)}/>
+    </div>
+    </>
   )
 }
 
