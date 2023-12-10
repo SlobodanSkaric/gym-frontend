fea66b60c1bc1aa726100c6ef506e3ae233b5407
Slobodan <slobodan.skaric@gmail.com>
2023-12-10 11:08:32 +0100

    Small change

--- src/router.jsx
+++ src/router.jsx
@@ -15,6 +15,7 @@ const router = createBrowserRouter([
             {
                 path: "user",
                 element: <UserLayout/>
+                ch
             },
             {
                 path: "coach",
